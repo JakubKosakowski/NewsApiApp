ef856fa2757dc6d71a5611eae560b18fa712c107
JakubKosakowski <jkosakowski602@gmail.com>
2024-11-23 14:46:18 +0100

    Create news country selection

--- src/index.php
+++ src/index.php
@@ -5,11 +5,21 @@ require "lib/install.php";
 
 session_start();
 
-// use jcobhams\NewsApi\NewsApi;
+use jcobhams\NewsApi\NewsApi;
 
-// $newsapi = new NewsAPI(getApiKey());
+$newsapi = new NewsAPI(getApiKey());
+
+$selectedCountry="us";
+if(isset($_GET['country'])) {
+  $selectedCountry = $_GET['country'];
+}
+
 // $trumpHeadlines = $newsapi->getTopHeadLines($q="trump");
 // var_dump($trumpHeadlines->articles);
+
+$usTopHeadlines = $newsapi->getTopHeadLines($country=$selectedCountry);
+$articles = $usTopHeadlines->articles;
+var_dump($articles[0]);
 
 ?>
 <!DOCTYPE html>
@@ -24,5 +34,13 @@ session_start();
     <?php require "../src/templates/navbar.php"; ?>
     <h1>Hello, world!</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <div class="container">
+      <?php foreach($articles as $article): ?>
+        <?php if($article->title != "[Removed]"): ?>
+          <h1><?=$article->title?></h1>
+          <h3><?=$article->source->name?></h3>
+        <?php endif ?>
+      <?php endforeach ?>
+    </div>
   </body>
 </html>
--- src/templates/navbar.php
+++ src/templates/navbar.php
@@ -1,29 +1,47 @@
+<?php
+$countries = $newsapi->getCountries();
+
+?>
 <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/">NewsApiApp</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="/about.php">About</a>
-          </li>
-        </ul>
-        <?php if(isLoggedIn()): ?>
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="/logout.php">Logout</a>
-                  </li>
-            </ul>
-        <?php else: ?>
-            <ul class="navbar-nav">
-                <a class="nav-link" href="/login.php"><button class="btn btn-outline-success">Login</button></a>
-            </ul>
-            <ul class="navbar-nav">
-              <a class="nav-link" href="/register.php"><button class="btn btn-outline-success">Register</button></a>
-            </ul>
-        <?php endif ?>
-      </div>
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">NewsApiApp</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="/about.php">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <button class="btn btn-primary dropdown-toggle" type="button" id="country" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src=<?="https://newsapi.org/images/flags/$selectedCountry.svg"?> width="20" height="20" class="icon loaded"?>
+          </button>
+          <ul class="dropdown-menu" id="country_list" aria-labelledby="country" style="min-width: 400px;">
+            <?php foreach($countries as $country): ?>
+              <li style="display: inline-block">
+                <a class="dropdown-item" href="?country=<?=$country?>">
+                  <img src=<?="https://newsapi.org/images/flags/$country.svg"?> width="20" height="20" class="icon loaded"?>
+                </a>
+              </li>
+            <?php endforeach ?>
+          </ul>
+        </li>
+      </ul>
+      <?php if(isLoggedIn()): ?>
+          <ul class="navbar-nav">
+              <li class="nav-item">
+                  <a class="nav-link" href="/logout.php">Logout</a>
+                </li>
+          </ul>
+      <?php else: ?>
+          <ul class="navbar-nav">
+              <a class="nav-link" href="/login.php"><button class="btn btn-outline-success">Login</button></a>
+          </ul>
+          <ul class="navbar-nav">
+            <a class="nav-link" href="/register.php"><button class="btn btn-outline-success">Register</button></a>
+          </ul>
+      <?php endif ?>
     </div>
-  </nav>
+  </div>
+</nav>
